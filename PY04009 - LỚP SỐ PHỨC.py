eb57d784cd3d96fbb4bcb8f28b286240958951ae
devNangAnh <nanganha10@gmail.com>
2023-01-02 14:23:33 +0700

    January 1st

---
+++ PY04009 - LỚP SỐ PHỨC.py
@@ -0,0 +1,28 @@
+class Complex:
+    def __init__(self, a, bi):
+        self.a = a
+        self.bi = bi
+
+    def cong(self, z2):
+        a = self.a + z2.a
+        bi = self.bi + z2.bi
+        return Complex(a, bi)
+
+    def nhan(self, z2):
+        a = self.a * z2.a - self.bi * z2.bi
+        bi = self.a * z2.bi + self.bi * z2.a
+        return Complex(a, bi)
+
+    def toSting(self):
+        if self.bi < 0:
+            return f"{self.a} - {abs(self.bi)}i"
+        else:
+            return f"{self.a} + {abs(self.bi)}i"
+
+for _ in range(int(input())):
+    a, bi, c, di = map(int, input().split())
+    A = Complex(a, bi)
+    B = Complex(c, di)
+    C = A.cong(B).nhan(A)
+    D = A.cong(B).nhan(A.cong(B))
+    print(C.toSting(), D.toSting(), sep=', ')
