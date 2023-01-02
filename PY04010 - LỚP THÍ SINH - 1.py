48664c02e69ae3467c05ac3ea1e4a150a6080ed0
devNangAnh <nanganha10@gmail.com>
2023-01-02 15:12:24 +0700

    January 2st

---
+++ PY04010 - LỚP THÍ SINH - 1.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, ten, ngaysinh, mon1, mon2, mon3):
+        self.ten = ten
+        self.ngaysinh = ngaysinh
+        self.mon1 = float(mon1)
+        self.mon2 = float(mon2)
+        self.mon3 = float(mon3)
+        self.tong = self.mon1 + self.mon2 + self.mon3
+
+    def toString(self):
+        return f'{self.ten} {self.ngaysinh} {self.tong}'
+
+ten, ngaysinh, mon1, mon2, mon3 = input(), input(), input(), input(), input()
+print(Student(ten, ngaysinh, mon1, mon2, mon3).toString())
