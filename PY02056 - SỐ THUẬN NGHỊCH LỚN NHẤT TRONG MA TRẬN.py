09fb05f529bf21a49caed71cb468fc0bf7244799
devNangAnh <nanganha10@gmail.com>
2023-01-05 00:04:29 +0700

    January 5th

---
+++ PY02056 - SỐ THUẬN NGHỊCH LỚN NHẤT TRONG MA TRẬN.py
@@ -0,0 +1,17 @@
+A, res = [], 0
+n, m = map(int, input().split())
+
+for i in range(n):
+    A.extend(list(map(int, input().split())))
+
+for i in A:
+    if i > 9 and str(i) == str(i)[::-1] and i > res:
+        res = i
+
+if res:
+    print(res)
+    for i, n in enumerate(A):
+        if n == res:
+            print(f"Vi tri [{i // m}][{i % m}]")
+else:
+    print("NOT FOUND")
