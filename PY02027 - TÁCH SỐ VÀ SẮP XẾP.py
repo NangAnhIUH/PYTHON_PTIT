e8a6130e327e71f40c1c90803af5611082341a77
devNangAnh <nanganha10@gmail.com>
2023-01-07 00:51:07 +0700

    January 7th

--- PY02027 - TÁCH SỐ VÀ SẮP XẾP.py
+++ PY02027 - TÁCH SỐ VÀ SẮP XẾP.py
@@ -6,4 +6,3 @@ patern = "[0-9]+"
 for _ in range(int(input())):
     A.extend(list(map(int, re.findall(patern, input()))))
 print(*sorted(A), sep="\n")
-
---
+++ PY02032 - LIỆT KÊ CÁC SỐ CÓ HAI CHỮ SỐ TĂNG DẦN.py
@@ -0,0 +1,6 @@
+D, s = {}, input()
+for i in range(0, len(s)-1, 2):
+    n = s[i:i+2]
+    D[n] = D.get(n, 0) + 1
+for i in sorted(D):
+    print(i, end=" ")
